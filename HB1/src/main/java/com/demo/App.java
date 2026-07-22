b68f523bb18475555facefd76a82ae54cd14331f
Suyash <patilsuyash7066@gmail.com>
2026-07-22 23:59:13 +0530

    second commit

--- HB1/src/main/java/com/demo/App.java
+++ HB1/src/main/java/com/demo/App.java
@@ -1,5 +1,12 @@
 package com.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.model.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Configuration con= new Configuration().configure();
+        SessionFactory sf = con.buildSessionFactory();
+        Session session =sf.openSession();
+        Transaction tx=session.beginTransaction();
+        
+        Student s1= new Student();
+        s1.setName("suyash");
+        s1.setCity("pune");
+        s1.setPercentage(90);
+        session.save(s1);
+        
+        tx.commit();
+        session.close();  
     }
 }
